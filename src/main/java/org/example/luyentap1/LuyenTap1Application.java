6838e83e7b7114d4fe5b0dfb0e38f2a17f2cca47
LeTuanAnh210 <letuananh21007@gmail.com>
2025-10-06 12:00:15 +0700

    Setup

---
+++ src/main/java/org/example/luyentap1/LuyenTap1Application.java
@@ -0,0 +1,14 @@
+package org.example.luyentap1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuyenTap1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuyenTap1Application.class, args);
+        System.out.println("Running....");
+    }
+
+}
